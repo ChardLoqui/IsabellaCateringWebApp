a3b0985f9fe19ec91e52fdd5a080176091dd6d6a
Richard Almonte <90769031+ChardLoqui@users.noreply.github.com>
2026-03-27 11:33:10 +0800

    Merge branch 'chard(22-2-2026)' of https://github.com/ChardLoqui/IsabellaCateringWebApp into chard(22-2-2026)

--- IsabellaCateringWebApp/Scripts/HolyScripts/Controller.js
+++ IsabellaCateringWebApp/Scripts/HolyScripts/Controller.js
@@ -125,6 +125,8 @@
                 $scope.firstName = ''; $scope.lastName = ''; $scope.email = '';
                 $scope.password = ''; $scope.confirmPassword = ''; $scope.permissionID = '';
                 $scope.addUserForm.$setPristine();
+                document.getElementById('firstNameAddCount').innerText = '0 / 30';
+                document.getElementById('lastNameAddCount').innerText = '0 / 30';
                 document.getElementById('emailAddCount').innerText = '0 / 50';
                 document.getElementById('passAddCount').innerText = '0 / 20';
                 $scope.getUsersData(); //refresh table 
@@ -243,6 +245,37 @@
     $scope.lastPage = function () {
         $scope.currentPage = $scope.numberOfPages();
     };
+
+    // func ng search and pagination 
+    $scope.logsData = [];
+    $scope.searchText = "";
+    $scope.appliedSearch = "";
+    $scope.currentPage = 1;
+    $scope.pageSize = 8; 
+
+    // search
+    $scope.searchLogs = function () {
+        $scope.appliedSearch = $scope.searchText;
+        $scope.currentPage = 1;
+    };
+
+    // pagination
+    $scope.numberOfPages = function () {
+        if (!$scope.logsData) return 1;
+        const filtered = $scope.$eval("logsData | filter:appliedSearch");
+        return Math.ceil(filtered.length / $scope.pageSize) || 1;
+    };
+
+    $scope.setPage = function (page) {
+        if (page >= 1 && page <= $scope.numberOfPages()) {
+            $scope.currentPage = page;
+        }
+    };
+    // double arrow para sa last page
+    $scope.lastPage = function () {
+        $scope.currentPage = $scope.numberOfPages();
+    };
+
 
 
 });
